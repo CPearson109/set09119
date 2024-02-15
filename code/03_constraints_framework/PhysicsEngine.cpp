fc32923242fe057f2c897f0d1bc19330429fce1d
CPearson109 <123629826+CPearson109@users.noreply.github.com>
2024-02-15 18:28:59 +0000

    2d Structure working

--- code/03_constraints_framework/PhysicsEngine.cpp
+++ code/03_constraints_framework/PhysicsEngine.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 const glm::vec3 GRAVITY = glm::vec3(0, -9.81, 0);
 
-const int gridWidth = 3;
-const int gridHeight = 3;
+const int gridWidth = 4;
+const int gridHeight = 10;
 
 void SymplecticEuler(Particle& pobj, vec3 totalForce, vec3 totalImpulse, float mass, float dt)
 {
@@ -30,26 +30,26 @@ void SymplecticEuler(Particle& pobj, vec3 totalForce, vec3 totalImpulse, float m
 
 vec3 CollisionImpulse(Particle& pobj, const std::vector<Particle>& particles, float groundHeight, float wallXPos, float wallZPos, float coefficientOfRestitution, vec3 impulse)
 {
-	vec3 pos = pobj.Position();
-	vec3 vel = pobj.Velocity();
-
-	// Ground collision
-	if (pos.y <= groundHeight && vel.y < 0) {
-		vec3 groundNormal = vec3(0.0f, 1.0f, 0.0f);
-		impulse -= pobj.Mass() * dot(vel, groundNormal) * groundNormal * (1 + coefficientOfRestitution);
-	}
-
-	// Wall collisions - X-axis walls
-	if ((pos.x <= -wallXPos && vel.x < 0) || (pos.x >= wallXPos && vel.x > 0)) {
-		vec3 wallXNormal = vec3(pos.x < 0 ? 1.0f : -1.0f, 0.0f, 0.0f);
-		impulse -= pobj.Mass() * dot(vel, wallXNormal) * wallXNormal * (1 + coefficientOfRestitution);
-	}
-
-	// Wall collisions - Z-axis walls
-	if ((pos.z <= -wallZPos && vel.z < 0) || (pos.z >= wallZPos && vel.z > 0)) {
-		vec3 wallZNormal = vec3(0.0f, 0.0f, pos.z < 0 ? 1.0f : -1.0f);
-		impulse -= pobj.Mass() * dot(vel, wallZNormal) * wallZNormal * (1 + coefficientOfRestitution);
-	}
+	//vec3 pos = pobj.Position();
+	//vec3 vel = pobj.Velocity();
+	//
+	//// Ground collision
+	//if (pos.y <= groundHeight && vel.y < 0) {
+	//	vec3 groundNormal = vec3(0.0f, 1.0f, 0.0f);
+	//	impulse -= pobj.Mass() * dot(vel, groundNormal) * groundNormal * (1 + coefficientOfRestitution);
+	//}
+	//
+	//// Wall collisions - X-axis walls
+	//if ((pos.x <= -wallXPos && vel.x < 0) || (pos.x >= wallXPos && vel.x > 0)) {
+	//	vec3 wallXNormal = vec3(pos.x < 0 ? 1.0f : -1.0f, 0.0f, 0.0f);
+	//	impulse -= pobj.Mass() * dot(vel, wallXNormal) * wallXNormal * (1 + coefficientOfRestitution);
+	//}
+	//
+	//// Wall collisions - Z-axis walls
+	//if ((pos.z <= -wallZPos && vel.z < 0) || (pos.z >= wallZPos && vel.z > 0)) {
+	//	vec3 wallZNormal = vec3(0.0f, 0.0f, pos.z < 0 ? 1.0f : -1.0f);
+	//	impulse -= pobj.Mass() * dot(vel, wallZNormal) * wallZNormal * (1 + coefficientOfRestitution);
+	//}
 
 	////Particle collisions
 	//for (const Particle& other : particles) {
@@ -96,7 +96,7 @@ void PhysicsEngine::Init(Camera& camera, MeshDb& meshDb, ShaderDb& shaderDb) {
 			particle.SetMesh(mesh);
 			particle.SetShader(defaultShader);
 			particle.SetColor(vec4(1.0f, 0.0f, 0.0f, 1.0f));
-			particle.SetPosition(vec3(0.0f + x + y, 10.0f - y, 0.0f));
+			particle.SetPosition(vec3(-2.0f + (x * 0.3) + (y * 0.2), 10.0f - (y * 0.3), 0.0f));
 			particle.SetScale(vec3(0.1f));
 			particle.SetVelocity(vec3(0.0f, 0.0f, 0.0f));
 			particle.SetMass(1.0f);
@@ -120,8 +120,8 @@ void PhysicsEngine::Task1Update(float deltaTime, float totalTime) {
 	int index = 0;
 
 	float dampingCoefficient = 0.1f;
-	float springConstant = 10.0f;
-	float restLength = 1.0f;
+	float springConstant = 40.0f;
+	float restLength = 0.3f;
 
 	for (Particle& particle : particles) {
 
@@ -141,50 +141,26 @@ void PhysicsEngine::Task1Update(float deltaTime, float totalTime) {
 					int index = y * gridWidth + x;
 					Particle& particle = particles[index];
 
-					// above
-					if (y > 0) {
-						Force::Hooke(particle, particles[(y - 1) * gridWidth + x], restLength, springConstant, dampingCoefficient);
-						//cout << index << " - " << (y - 1) * gridWidth + x << endl;
-					}
-
 					// below
 					if (y < gridHeight - 1) {
 						Force::Hooke(particle, particles[(y + 1) * gridWidth + x], restLength, springConstant, dampingCoefficient);
-						//cout << index << " - " << (y + 1) * gridWidth + x << endl;
-					}
-
-					// left
-					if (x > 0) {
-						Force::Hooke(particle, particles[y * gridWidth + (x - 1)], restLength, springConstant, dampingCoefficient);
-						//cout << index << " - " << y * gridWidth + (x - 1) << endl;
 					}
 
 					// right
 					if (x < gridWidth - 1) {
 						Force::Hooke(particle, particles[y * gridWidth + (x + 1)], restLength, springConstant, dampingCoefficient);
-						//cout << index << " - " << y * gridWidth + (x + 1) << endl;
 					}
 
-					// top left
-					if (x > 0 && y > 0) {
-						Force::Hooke(particle, particles[(y - 1) * gridWidth + (x - 1)], restLength, springConstant, dampingCoefficient);
-						cout << index << " - " << y - 1 * gridWidth + (x + 1) << endl;
-					}
-				
-
-					//top right
-					if (x > gridWidth - 1 && y > 0) {
-
+					// bottom left
+					if (x > 0 && y < gridHeight - 1) {
+						float restLength = 0.424;
+						Force::Hooke(particle, particles[(y + 1) * gridWidth + (x - 1)], restLength, springConstant, dampingCoefficient);
 					}
 
-					//bottom left
-					if (y < gridHeight - 1 && x > 0) {
-
-					}
-
-					//bottom right
-					if (y < gridHeight - 1 && y > 0) {
-
+					// bottom right
+					if (x < gridWidth - 1 && y < gridHeight - 1) {
+						float restLength = 0.424;
+						Force::Hooke(particle, particles[(y + 1) * gridWidth + (x + 1)], restLength, springConstant, dampingCoefficient);
 					}
 				}
 			}
